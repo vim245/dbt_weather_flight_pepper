b9b1f427d3289990bec0c704d2c9c7cdad7461f2
vimala <vimalabhandari15@gmail.com>
2024-11-11 18:12:39 +0100

    jjjjjj

--- models/prep/prep_airports.sql
+++ models/prep/prep_airports.sql
@@ -1,15 +1,15 @@
 
 WITH airports_reorder AS (
 SELECT faa
-        ,name
-        ,city
-        ,country
-        ,region
-        ,lat
-        ,lon
-        ,alt
-        ,tz
-        ,dst
+    ,name
+    ,city
+    ,country
+    ,region
+    ,lat
+    ,lon
+    ,alt
+    ,tz
+    ,dst
 FROM {{ref('staging_airports')}}
 )
 SELECT * FROM airports_reorder
--- models/prep/prep_flights.sql
+++ models/prep/prep_flights.sql
@@ -1,29 +1,29 @@
-WITH flights_one_month AS (
+WITH flights_january AS (
 SELECT * 
-FROM {{ref('staging_flights_one_month')}}
+FROM {{ref('staging_flights_january')}}
 ),
 flights_cleaned AS(
 SELECT flight_date::DATE
         ,TO_CHAR(dep_time, 'fm0000')::TIME AS dep_time
-        ,... AS sched_dep_time
+        ,TO_CHAR(sched_dep_time, 'fm0000')::TIME AS sched_dep_time
         ,dep_delay
-        ,(dep_delay * '1 minute'::INTERVAL) AS dep_delay_interval
-        ,...::TIME AS arr_time
-        ,...::TIME AS sched_arr_time
+        ,(dep_delay * '1 minute'::interval) AS dep_delay_interval
+        ,TO_CHAR(arr_time, 'fm0000')::TIME AS arr_time
+        ,TO_CHAR(sched_arr_time, 'fm0000')::TIME AS sched_arr_time
         ,arr_delay
-        ,(...) AS arr_delay_interval
+        ,(arr_delay * '1 minute'::interval) AS arr_delay_interval
         ,airline
         ,tail_number
         ,flight_number
         ,origin
         ,dest
         ,air_time
-        ,(...) AS air_time_interval
+        ,(air_time * '1 minute'::INTERVAL) AS air_time_interval
         ,actual_elapsed_time
-        ,(...) AS actual_elapsed_time_interval
-        ,(distance * 0.621371)::NUMERIC(6,2) AS distance_km 
+        ,(actual_elapsed_time * '1 minute'::INTERVAL) AS actual_elapsed_time_interval
+        ,(distance * 0.621371)::NUMERIC(6,2) AS distance_km
         ,cancelled
         ,diverted
-FROM flights_one_month
+FROM flights_january
 )
 SELECT * FROM flights_cleaned
