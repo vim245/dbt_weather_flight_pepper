58ad88c5edeea63b1367474cb6621da60ae0ad7b
vimala <vimalabhandari15@gmail.com>
2024-11-11 15:36:40 +0100

    VBb

---
+++ models/staging/staging_airport.sql
@@ -0,0 +1,8 @@
+
+WITH airports_regions_join AS (
+    SELECT * 
+    FROM {{source('flights_data', 'airports')}}
+    LEFT JOIN {{source('flights_data', 'regions')}}
+    USING (country)
+)
+SELECT * FROM airports_regions_join
