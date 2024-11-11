89954c70eb84fe3989230e737993ec01d37413f8
vimala <vimalabhandari15@gmail.com>
2024-11-11 16:03:52 +0100

    VB_airport

---
+++ models/prep/Prep_airport.sql
@@ -0,0 +1,15 @@
+
+WITH airports_reorder AS (
+SELECT faa
+        ,name
+        ,city
+        ,country
+        ,region
+        ,lat
+        ,lon
+        ,alt
+        ,tz
+        ,dst
+FROM {{ref('staging_airports')}}
+)
+SELECT * FROM airports_reorder
